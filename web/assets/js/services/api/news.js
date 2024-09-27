81acbf4a05c3af18a3bfd42fa0add222e6f5b9f1
Felipe Biel <felipe.biel@t10.digital>
2024-09-27 11:06:33 -0300

    feat: Carregando a listagem de grupo de noticias

--- web/assets/js/services/api/news.js
+++ web/assets/js/services/api/news.js
@@ -7,3 +7,10 @@ export async function getNews(page = 1) {
 
     return data.filter(el => el.type === 'materia');
 }
+
+export async function getNewsGroup() {
+    let response = await fetch(`${BASE_URL}${ENDPOITS.FEED}/${ENDPOITS.PAGE}/1`);
+    let data = await response.json();
+
+    return data.filter(el => el.type === 'agrupador-materia');
+}
