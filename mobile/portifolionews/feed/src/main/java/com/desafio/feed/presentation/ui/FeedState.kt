b38b6aa583e755168f6d9a04688e868539237546
Gustavo Matheus <maestrom217@gmail.com>
2024-12-06 13:15:22 -0300

    Create new widget to pull to refresh and add in post list

--- mobile/portifolionews/feed/src/main/java/com/desafio/feed/presentation/ui/FeedState.kt
+++ mobile/portifolionews/feed/src/main/java/com/desafio/feed/presentation/ui/FeedState.kt
@@ -6,7 +6,8 @@ import com.desafio.feed.presentation.ui.dto.NewsDto
 sealed class FeedState {
     object START : FeedState()
     object LOADING : FeedState()
-    data class SUCCESS(val feedDTO: FeedDTO) : FeedState()
+    data class REFRESH(val isRefreshing: Boolean) : FeedState()
+    data class SUCCESS(val feedDTO: FeedDTO, val isRefreshing: Boolean) : FeedState()
     data class FETCH(val feedNews: List<NewsDto>) : FeedState()
     data class FAILURE(val message: String) : FeedState()
 }
